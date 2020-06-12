bcf2327e950fefed38f8fdd59fedab244a0456b6
xiaoyang <1764974647@qq.com>
2020-06-12 13:50:10 +0800

    完成了编辑修改

--- assets/js/article/category.js
+++ assets/js/article/category.js
@@ -14,11 +14,15 @@ function renderCatgory() {
 
 
 $(function () {
+    var addIndex  //添加的弹出层
+    var editIndex  //编辑的弹出层
+    var form = layui.form
+
     // -------------------获取列表数据,渲染到页面---------
     renderCatgory()
 
     // ---------------------添加类别弹出层-----------------------
-    var addIndex
+
     $('.layui-card-header button').on('click', function () {
         // 使用layer.open()实现弹出层
         addIndex = layer.open({
@@ -79,4 +83,59 @@ $(function () {
             layer.close(index);
         })
     })
+    // --------------- 点击 编辑 ，弹层，为表单赋值 -------------------------
+    $('body').on('click', '.edit', function () {
+        // 获取三个自定义属性
+        var id = $(this).attr('data-id')
+        var name = $(this).attr('data-name')
+        var alias = $(this).attr('data-alias')
+        // h5中，提供了dataset属性，专门用于获取对象的data-xxx属性值
+        // var data = this.dataset; // dataset是dom属性，所以使用dom对象this
+        editIndex = layer.open({
+            type: 1,
+            title: '编辑类别',
+            content: $('#tpl-edit').html(),
+            area: ['500px', '250px'],
+            success: function () {
+                // 弹层之后，执行这个函数， 
+                // 快速为表单赋值
+                // edit-form是表单的lay-filter属性值
+                form.val('edit-form', {
+                    id: id,
+                    name: name,
+                    alias: alias
+                })
+                // form.val('edit-form', JSON.parse(JSON.stringify(data)))
+            }
+        });
+    });
+
+    // --------------- 点击弹出框确认，提交数据，完成修改 ------
+    $('body').on('submit', '#edit-form', function (e) {
+        e.preventDefault()
+        // var data = $(this).serialize()
+        // data = data.replace('id=', 'Id=')
+        // data = 'I' + data.substr(1)
+
+        var data = $(this).serializeArray()
+        data[0].name = 'Id'
+        // console.log(data)
+        $.ajax({
+            type: 'POST',
+            url: '/my/article/updatecate',
+            data: data,
+            success: function (res) {
+                layer.msg(res.message)
+                if (res.status === 0) {
+                    // 重新渲染
+                    renderCatgory()
+                    // 关闭弹出层
+                    layer.close(editIndex)
+                }
+            }
+        })
+    })
+
+
 })
+
